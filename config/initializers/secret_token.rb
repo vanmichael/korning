8ce8fcca92f5f7461031a20d1709c0fcd601306b
Johnny Boursiquot <jboursiquot@gmail.com>
2013-12-12 19:54:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Korning::Application.config.secret_key_base = '12aa3b57e0a45372e619a63f869c2fb3cf7b18ab7803d89e56ce386369b1b1a9734e3ed69a2e7cc5f7ecea72b22408d6a64dd2546c3fad244c2d19e554d7f45f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Korning::Application.config.session_store :cookie_store, key: '_korning_session'
